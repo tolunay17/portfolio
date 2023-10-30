ca760b822562fe46e9c603beed88e113e75c8960
unknown <tolunay17>
2023-10-30 09:14:15 +0100

    changed a few things

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
-
 const WorkItem = ({year, title, position, description}) => {
   return (
-    <ul className='flex flex-col md:flex-row relative border-l border-stone-200'>
+    <ul className='flex flex-col md:flex-row relative border-l border-blue-200'>
         <li className='mb-10 ml-4'>
-            <div className='absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white'/>
+            <div className='absolute w-3 h-3 bg-blue-400 rounded-full mt-1.5 -left-1.5 border-white'/>
             <p className='flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm'>
-                <span className='inline-block px-2 py-1 font-semibold text-white bg-[#000] rounded-md'>{year}</span>
-                <span className='text-lg font-semibold text-[#000]'>{title}</span>
+                <span className='inline-block px-3 py-1 font-bold text-white bg-[#000] rounded-md'>{year}</span>
+                <span className='text-lg font-bold text-[#000]'>{title}</span>
                 <span className='my-1 text-sm font-normal leading-none text-stone-400'>{position}</span>
             </p>
             <p className='my-2 text-base font-normal text-stone-500'>{description}</p>
@@ -15,5 +13,4 @@ const WorkItem = ({year, title, position, description}) => {
     </ul>
   )
 }
-
 export default WorkItem
